a2bfc6c59fad5e64d850490fda428010ca6f916a
willian emboava <w.emboava.o@gmail.com>
2020-08-03 16:56:50 -0300

    readme updated

--- src/pages/side/index.tsx
+++ src/pages/side/index.tsx
@@ -75,13 +75,7 @@ const Side: React.FC = () => {
     <Container theme={currentSideTheme}>
       <div className="back-container">
         <button onClick={() => history.push('/')} type="button">
-          <svg
-            width="31"
-            height="26"
-            viewBox="0 0 31 26"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
+          <svg width="31" height="26" viewBox="0 0 31 26" fill="none">
             <path d="M31.0001 11.3333H6.69006L15.5117 2.51166L13.1551 0.154999L0.310059 13L13.1551 25.845L15.5117 23.4883L6.69006 14.6667H31.0001V11.3333Z" />
           </svg>
           <span>back</span>
