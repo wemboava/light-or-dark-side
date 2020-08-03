e541b9a6b34e8853efdbaca64b369c6a43416c06
willian emboava <w.emboava.o@gmail.com>
2020-08-03 17:12:34 -0300

    headers test

--- src/service/api.ts
+++ src/service/api.ts
@@ -2,6 +2,11 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://swapi.dev/api',
+  withCredentials: false,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  },
 });
 
 export default api;
